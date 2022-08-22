a55bd877c02f1a66f1c7573d8a6a8afe9724580a
James R <jimmer1609@gmail.com>
2022-08-22 21:54:16 +0900

    controller mapped

--- DungeonGame/src/main/java/Group1/DungeonGame/controllers/WeaponController.java
+++ DungeonGame/src/main/java/Group1/DungeonGame/controllers/WeaponController.java
@@ -1,7 +1,34 @@
 package Group1.DungeonGame.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import Group1.DungeonGame.models.weapons.Weapon;
+import Group1.DungeonGame.repositories.WeaponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class WeaponController {
+
+    @Autowired
+    WeaponRepository weaponRepository;
+
+    @GetMapping(value="/weapons")
+    public ResponseEntity<List<Weapon>> getAllWeapons() {
+        return new ResponseEntity<>(weaponRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/weapons/{id}")
+    public ResponseEntity<Optional<Weapon>> getWeapon(@PathVariable Long id) {
+        return new ResponseEntity<>(weaponRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/weapons")
+    public ResponseEntity<Weapon> createWeapon(@RequestBody Weapon weapon) {
+        weaponRepository.save(weapon);
+        return new ResponseEntity<>(weapon, HttpStatus.CREATED);
+    }
 }
