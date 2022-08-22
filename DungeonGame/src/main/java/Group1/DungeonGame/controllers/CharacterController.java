8f3dd66b20bed8fd2608c8b015dc3771c71d1b44
Kenny Wands <kjwands@gmail.com>
2022-08-22 16:49:06 +0100

    Weapon Issue

--- DungeonGame/src/main/java/Group1/DungeonGame/controllers/CharacterController.java
+++ DungeonGame/src/main/java/Group1/DungeonGame/controllers/CharacterController.java
@@ -1,13 +1,24 @@
 package Group1.DungeonGame.controllers;
 
+import Group1.DungeonGame.repositories.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Repository
-public interface CharacterController extends JpaRepository<Character, Long> {
-
+@RestController
+public class CharacterController {
+   @Autowired
+    CharacterRepository characterRepository;
+    @GetMapping(value ="/characters")
+    public ResponseEntity<List<Character>>  getAllCharacters(){
+        return new ResponseEntity<>(characterRepository.findAll(), HttpStatus.OK);
+    }
 
 
 }
---
+++ DungeonGame/src/test/java/characterTests/CharacterTest.java
@@ -0,0 +1,46 @@
+package characterTests;
+
+import Group1.DungeonGame.models.Character;
+import Group1.DungeonGame.models.weapons.Axe;
+import Group1.DungeonGame.models.weapons.Weapon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CharacterTest {
+    Weapon axe;
+    Character character;
+
+    @Before
+    public void beforeEach(){
+        axe = new Weapon("Barry",100);
+        character = new Character("Kevin", 100, 10,1, axe);
+    }
+
+    @Test
+    public void characterHasName(){
+        assertEquals("Kevin", character.getName());
+    }
+
+    @Test
+    public void characterHasHealth(){
+        assertEquals(100, character.getMaxHealth());
+    }
+    @Test
+    public void characterHasExp(){
+        assertEquals(10, character.getExp());
+    }
+    @Test
+    public void characterHasLevel(){
+        assertEquals(1, character.getLevel());
+    }
+    @Test
+    public void characterHasWeapon(){
+        assertEquals("Barry", character.getWeapon().getName());
+    }
+
+
+
+}
