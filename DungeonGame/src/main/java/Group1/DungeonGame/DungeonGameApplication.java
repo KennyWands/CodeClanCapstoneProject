7b2753a43bf5f8f73cbdcd0536f261c97a8ed373
Kenny Wands <kjwands@gmail.com>
2022-08-22 11:09:03 +0100

    initial commit

---
+++ DungeonGame/src/main/java/Group1/DungeonGame/DungeonGameApplication.java
@@ -0,0 +1,13 @@
+package Group1.DungeonGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonGameApplication.class, args);
+	}
+
+}
---
+++ DungeonGame/src/test/java/Group1/DungeonGame/DungeonGameApplicationTests.java
@@ -0,0 +1,13 @@
+package Group1.DungeonGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
